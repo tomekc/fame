ae4c35c413f0f8c37d72c3e27393e1c3e10df758
Gunter Hager <gunter@allaboutapps.at>
2016-03-08 15:43:44 +0100

    Implemented getting localization languages from Xcode project file.

--- fame.gemspec
+++ fame.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "nokogiri", "~> 1.0"
   spec.add_dependency "plist", "~> 3.0"
   spec.add_dependency "colorize", "~> 0.7"
+  spec.add_dependency "pbxplorer", "~> 1.0"
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/fame/XLIFF.rb
+++ lib/fame/XLIFF.rb
@@ -1,4 +1,5 @@
 require 'nokogiri' 		# to rewrite the XLIFF file
+require 'pbxplorer'     # to get localization languages from Xcode project file
 require 'colorize'		# colorful console output
 require_relative 'models'
 
@@ -6,55 +7,75 @@ module Fame
 
 	class XLIFF
 
-		def generate(language)
-			`xcodebuild -exportLocalizations -localizationPath . -exportLanguage #{language}`
+		def generate(path = '.', project)
+			languages_string = get_languages(project)
+				.map { |l| "-exportLanguage #{l}" }
+				.join(' ')
+
+			`xcodebuild -exportLocalizations -localizationPath #{path} -project #{project} #{languages_string}`
 		end
 
 		#
 		# Modifies all nodes that are related to ib_nodes
 		#
-		def update_trans_units(language, ib_nodes)
+		def update_trans_units(project, ib_nodes)
 			
-			# Read XLIFF
-			file = File.open("#{language}.xliff") 
-			doc = Nokogiri::XML(file)
-			units = doc.xpath('//xmlns:trans-unit')
-			file.close
+			get_languages(project).each do |language|
 
-			ib_nodes.each do |ib_node|
-				oid = ib_node.original_id
-				formatted_info = ib_node.formatted_info
+		        puts "Updating translation units for #{language}".blue
 
-				# Select nodes connected to oid
-				nodes = units.select do |unit| 
-					uid = unit['id'] rescue ""
-					uid.include?(oid)
-				end
+				# Read XLIFF
+				file = File.open("#{language}.xliff") 
+				doc = Nokogiri::XML(file)
+				units = doc.xpath('//xmlns:trans-unit')
+				file.close
 
-				# Update or remove nodes
-				nodes.each do |node|
-					if ib_node.i18n_enabled
-						note = node.xpath('xmlns:note')
-						note.children.first.content = formatted_info
-					else
-						node.remove
+				ib_nodes.each do |ib_node|
+					oid = ib_node.original_id
+					formatted_info = ib_node.formatted_info
+
+					# Select nodes connected to oid
+					nodes = units.select do |unit| 
+						uid = unit['id'] rescue ""
+						uid.include?(oid)
+					end
+
+					# Update or remove nodes
+					nodes.each do |node|
+						if ib_node.i18n_enabled
+							note = node.xpath('xmlns:note')
+							note.children.first.content = formatted_info
+						else
+							node.remove
+						end
+					end
+					if nodes.count > 0
+						if ib_node.i18n_enabled
+							puts "  ✔︎ ".green + "#{nodes.count} translation unit(s) ".black + "updated".green + " for #{oid} ".black + "#{formatted_info}".light_black
+						else 
+							puts "  ✔︎ ".green + "#{nodes.count} translation unit(s) ".black + "removed".red + " for #{oid} ".black + "#{formatted_info}".light_black
+						end
 					end
 				end
-				if nodes.count > 0
-					if ib_node.i18n_enabled
-						puts "  ✔︎ ".green + "#{nodes.count} translation unit(s) ".black + "updated".green + " for #{oid} ".black + "#{formatted_info}".light_black
-					else 
-						puts "  ✔︎ ".green + "#{nodes.count} translation unit(s) ".black + "removed".red + " for #{oid} ".black + "#{formatted_info}".light_black
-					end
-				end
+
+				# Write updated XLIFF
+				file = File.open("#{language}.xliff", "w") 
+				doc.write_xml_to(file)
+				file.close
+
 			end
 
-			# Write updated XLIFF
-			file = File.open("#{language}.xliff", "w") 
-			doc.write_xml_to(file)
-			file.close
+		end
+
+		private 
+
+		def get_languages(project)
+			project_file = XCProjectFile.new(project)
+			project_file.project['knownRegions'].select { |r| r != 'Base' }
 		end
 
 	end
+
+
 
 end
